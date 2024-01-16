b680b275391ab345588fa5bd2f67dd5d899164b8
mohammad asif <asifumer974@gmail.com>
2024-01-16 15:35:22 +0500

    list method

--- list.dart
+++ list.dart
@@ -3,7 +3,9 @@ void main() {
   // var list1 = [10, 15, 20, 25, 25, 23, 12];
   // print(list1);
 
-  var lst = [1, 2, 3, 4, 5];
+  // ..........list method...........
+
+  var lst = [1, 2, 3, 4, 5, 8, 7];
   var e1 = lst.first;
   var e2 = lst.last;
   var e3 = lst.elementAt(1);
